324895e2936da726be7f6537b6c7565e2c6e040b
Caitlin Craig <caitlincraig1016@gmail.com>
2023-02-22 19:54:32 -0600

    adding_correct_rating

--- scripts/app_trader_Caitlin.sql
+++ scripts/app_trader_Caitlin.sql
@@ -1,14 +1,30 @@
---Searching for apps that overlap stores - 553
+-- Query Highest rated games
 
---Trying to return highest rated games and lowest price. So far have avg rating between both stores as rating. Need to add on price
-SELECT DISTINCT a.name, ROUND(((a.rating + p.rating)/2),1) AS rating
-FROM app_store_apps AS a
-INNER JOIN play_store_apps AS p
-ON a.name = p.name
-ORDER BY rating DESC
+SELECT app_store.name, CAST(app_store.price AS MONEY),CAST(play_store.price AS MONEY), app_store.rating, play_store.rating
+FROM app_store_apps AS app_store
+INNER JOIN play_store_apps AS play_store
+ON app_store.name = play_store.name
+WHERE app_store.rating IS NOT NULL
+	AND play_store.rating IS NOT NULL
+ORDER by app_store.rating DESC
 
+-- Query how many games rated 5 are free?
+SELECT COUNT(*)
+FROM app_store_apps
+WHERE price = 0
+ -- 4056
 
+--Highest rated games that are free
+SELECT app_store.name, CAST(app_store.price AS MONEY) AS app_price,CAST(play_store.price AS MONEY) AS play_price, app_store.rating, play_store.rating
+FROM app_store_apps AS app_store
+INNER JOIN play_store_apps AS play_store
+ON app_store.name = play_store.name
+WHERE app_store.rating IS NOT NULL
+	AND play_store.rating IS NOT NULL
+	AND CAST(app_store.price AS MONEY) < '$1.00'
+	AND CAST(play_store.price AS MONEY)<'$1.00'
+ORDER by app_store.rating DESC
 
-
+--Need correct rating score.
 
 
